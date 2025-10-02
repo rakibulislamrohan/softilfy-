3da399d3c89134ae3827c00254bb2f534976b7f9
MD Rakibul Islam Rohan <121843284+rakibulislamrohan@users.noreply.github.com>
2025-10-02 21:48:13 +0600

    addsong
    added eight song for provied list

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("welcome to spotify");
+
+let songs = [
+    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"song/1.mp3", overPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+]
