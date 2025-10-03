fcfbf04e2b0a03c3548f89f6673d8254f655200d
MD Rakibul Islam Rohan <121843284+rakibulislamrohan@users.noreply.github.com>
2025-10-03 18:30:05 +0600

    js
    write lestining song with javascript

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 console.log("welcome to spotify");
+// intilize the song variable s
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = documnet.getElementById('myProgeressBar');
 
 let songs = [
-    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
-    {songName:"salam isque hae", filePath:"song/1.mp3", overPath:"covers/1.jpg"},
-    {songName:"salam isque hae", filePath:"song/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
+    {songName:"salam isque hae", filePath:"songs/1.mp3", coverPath:"covers/1.jpg"},
 ]
+
+// Audion element.play();
+
+// Handle play pose click
+masterPlay.addEventListener('click', () =>{
+    if(audioElement.paused||audioElement.currentTime<=0){
+        audioElement.play(); 
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');        
+    }
+})
+
+myProgressBar.addEventListener('timeUpdate', () => {
+    console.log('timeUpdate');
+    // update seek bar 
+
+})
