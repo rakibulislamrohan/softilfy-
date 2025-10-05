f329fdb356b127fb43787b76bc741927cce72e11
MD Rakibul Islam Rohan <121843284+rakibulislamrohan@users.noreply.github.com>
2025-10-05 19:51:58 +0600

    add
    add progress bar on nevagation on real time

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let songIndex = 0;
 let audioElement = new Audio('song/1.mp3');  
 let masterPlay = document.getElementById('masterPlay');
 //let myProgressBar = documnet.getElementById('myProgeressBar');
+let gif = document.getElementById('gif');
 
 let songs = [
     {songName:"salam-isque-hae", filePath:"song/1.mp3", coverPath:"cover/1.jpg"},
@@ -23,18 +24,22 @@ masterPlay.addEventListener('click', () =>{
     if(audioElement.paused||audioElement.currentTime<=0){
         audioElement.play(); 
         masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');        
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;         
     }
     else{
           audioElement.pause(); 
         masterPlay.classList.remove('fa-pause-circle');
-        masterPlay.classList.add('fa-play-circle');    
+        masterPlay.classList.add('fa-play-circle');  
+        gif.style.opacity = 0;  
     }
 })
 
-myProgressBar.addEventListener('timeupdate', () => {
+audioElement.addEventListener('timeupdate', () => {
     console.log('timeUpdate');
     // update seek bar 
-
+Progress = parseInt((audioElement.currentTime/audioElement.duration)* 100);
+console.log(Progress);
+myProgressBar.value = Progress; 
 })
  
